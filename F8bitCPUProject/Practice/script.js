96fd1e2bb8e1b91c1f5450404d655ede4afcf0cc
Aamir Sayyad <aamirsayyad6@gmail.com>
2026-09-16 10:42:36 +0530

    deleted files

--- F8bitCPUProject/Practice/script.js
+++
@@ -1,5 +0,0 @@
-const Prom= new Promise((resolve,reject)=>{
-    setTimeout(()=>{resolve("Hello World!")},3000);
-    }).then((data)=>console.log(data));
-
-
--- F8bitCPUProject/REACT_LEARNING/src/components/Cell.jsx
+++
@@ -1,9 +0,0 @@
-import './Cell.css';
-function Cell({color,size}){
-    return (
-        <div className={"board-cell "+ color} style={{height:`${size}px` , width:`${size}px`}}>
-            {color}
-        </div>
-    )
-}
-export default Cell
